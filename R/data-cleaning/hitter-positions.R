bb22eaa18b470d1458e9815d87f3ad4ec2b6baba
Jack Banks <jackb4@illinois.edu>
2022-12-07 15:08:51 -0600

    new folder for cleanliness

---
+++ R/data-cleaning/hitter-positions.R
@@ -0,0 +1,33 @@
+
+library(tidyverse)
+
+d_list = list()
+pos_vec = c("c", "1b", "2b", "3b", "ss",
+            "lf", "cf", "rf", "dh")
+
+
+for(i in 1:9) {
+  d_list[[i]] = read_csv(paste0("data/archived/fangraphs-",pos_vec[i],".csv"))
+  
+  d_list[[i]]$POS = toupper(pos_vec[i])
+}
+
+data = do.call("rbind", d_list)
+
+# original teams
+
+old = read_csv("data/archived/hitter-fangraphs-projections.csv")
+old = old %>% select(playerid, Team)
+
+data = {
+  data %>% 
+    select(-Team) %>% 
+    left_join(old, by = "playerid") %>% 
+    select(playerid, Name, POS, Team, G:`wRC+`,
+           Off, Def, WAR) %>% 
+    arrange(desc(WAR))
+}
+
+write_csv(data, "data/hitter-projections.csv")
+
+
---
+++ R/data-cleaning/scraper.R
@@ -0,0 +1,57 @@
+
+library(tidyverse)
+
+# Player market values
+
+url = rvest::read_html("https://www.spotrac.com/mlb/free-agents/available/")
+players = rvest::html_table(url)[[1]]
+
+readr::write_csv(players, "data/player-values.csv")
+
+# Team base salaries
+
+url = rvest::read_html("https://www.spotrac.com/spots/early-offseason-mlb-payroll-projections-1665/")
+teams = rvest::html_table(url)[[1]]
+
+readr::write_csv(teams, "data/team-caps.csv")
+
+# Prior season salaries
+
+l = list(d20 = NULL, d21 = NULL, d22 = NULL)
+
+for(i in 1:3) {
+  u = rvest::read_html(paste0("https://www.spotrac.com/mlb/payroll/",
+                              2019 + i, "/"))
+  d = rvest::html_table(u)[[1]]
+  
+  d$Season = 2019 + i
+  
+  names(d)[c(5, 10)] = c("Active Roster", "Total Payroll")
+  
+  d = dplyr::filter(d, Team != "League Average")
+  
+  l[[i]] = d
+}
+
+past_sal = rbind(l$d20, l$d21, l$d22)
+
+past_sal$`Total Payroll` = as.numeric(str_remove_all(past_sal$`Total Payroll`, "[$,]"))
+
+past_sal = {
+  past_sal %>% 
+    mutate(TaxCap = ifelse(Season == 2020, 208000000,
+                           ifelse(Season == 2021, 210000000,
+                                  230000000))) %>% 
+    mutate(wPayroll = `Total Payroll`/TaxCap * 100)
+}
+
+readr::write_csv(past_sal, "data/team-past-salaries.csv")
+
+
+# Franchise Values
+
+u = rvest::read_html("https://en.wikipedia.org/wiki/Forbes_list_of_the_most_valuable_MLB_clubs")
+franch = rvest::html_table(u)[[3]]
+
+readr::write_csv(franch, "data/franchise-values.csv")
+
